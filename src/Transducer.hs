8b830ee8b64bdba74c403034ea08ffe42ea5b6df
Lars Petersen <info@lars-petersen.net>
2016-03-30 23:02:10 +0200

    Got the data structure right.

--- src/Transducer.hs
+++ src/Transducer.hs
@@ -1,1 +1,13 @@
 module Control.Transducer where
+
+newtype Source m o = Source { pull :: m (Maybe (o, Source m o)) }
+
+type Transducer m i o = Source m i -> Source m o
+
+consume :: Monad m => Source m o -> m [o]
+consume source =
+  pull source >>= maybe (return []) (\x-> (fst x:) <$> consume (snd x))
+
+drain :: Monad m => Source m o -> m ()
+drain source =
+  pull source >>= maybe (return ()) (drain . snd)
