5eadcd860cc5d87313d322c1f0459b8d1f1bfd57
Lars Petersen <info@lars-petersen.net>
2016-03-31 18:36:07 +0200

    Implemented tail and peek.

--- src/Data/Source.hs
+++ src/Data/Source.hs
@@ -1,30 +1,26 @@
 {-# LANGUAGE TypeFamilies #-}
 module Data.Source where
 
-import Prelude hiding (head)
+import Prelude hiding (head, tail)
 import System.IO
 
-newtype Source m o = Source { pull :: m (Maybe (o, Source m o)) }
+newtype Source     m o   = Source { pull :: m (Maybe (o, Source m o)) }
+type    Transducer m i o = Source m i -> Source m o
 
-type Transducer m i o = Source m i -> Source m o
+push             :: Monad m => o -> Source m o -> Source m o
+push o source     = Source $ return $ Just (o, source)
 
-class Origin a where
-  type (Context a) :: * -> *
-  type (Output a)  :: *
-  toSource :: a -> Source (Context a) (Output a)
-
-instance Origin Handle where
-  type (Context Handle) = IO
-  type (Output Handle)  = Char
-  toSource = undefined
-
-consume :: Monad m => Source m o -> m [o]
-consume source =
-  pull source >>= maybe (return []) (\x-> (fst x:) <$> consume (snd x))
+exhausted        :: Monad m => Source m o
+exhausted         = Source $ return Nothing
 
 drain :: Monad m => Source m o -> m ()
 drain source =
   pull source >>= maybe (return ()) (drain . snd)
+
+peek  :: Monad m => Source m o -> m (Maybe (o, Source m o))
+peek source = (f <$>) <$> pull source
+  where
+    f (o, source') = (o, push o source')
 
 take :: Monad m => Int -> Transducer m [i] [i]
 take i source = Source $ pull source >>= \m-> case m of
@@ -32,13 +28,17 @@ take i source = Source $ pull source >>= \m-> case m of
   Just xs -> undefined
 
 head :: Monad m => Transducer m [i] i
-head source = pull source >>= \m-> case m of
-  --Just ([],   source') -> head source'
-  --Just (x:xs, source') -> Source $ return $ Just (x, pushBack xs source')
-  _                    -> exhausted
+head source = Source $ pull source >>= maybe (return Nothing) f
+  where
+    f ([],   source') = pull $ head source'
+    f (x:xs, source') = return $ Just (x, head $ push xs source')
 
-pushBack         :: Monad m => o -> Source m o -> Source m o
-pushBack o source = Source $ return $ Just (o, source)
+tail :: Monad m => Transducer m [i] [i]
+tail source = Source $ pull source >>= maybe (return Nothing) f
+  where
+    f (_:xs, source') = pull $ push xs source'
+    f ([],   source') = pull $ tail source'
 
-exhausted        :: Monad m => Source m o
-exhausted         = Source $ return Nothing
+consume :: Monad m => Source m o -> m [o]
+consume source =
+  pull source >>= maybe (return []) (\x-> (fst x:) <$> consume (snd x))
