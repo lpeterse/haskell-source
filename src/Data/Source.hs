5883f4664ca3b59533c179d0b9218d5884c98e7d
Lars Petersen <info@lars-petersen.net>
2016-03-31 16:44:52 +0200

    New function pushBack and exhausted.

--- src/Data/Source.hs
+++ src/Data/Source.hs
@@ -1,9 +1,12 @@
 {-# LANGUAGE TypeFamilies #-}
 module Data.Source where
 
+import Prelude hiding (head)
 import System.IO
 
 newtype Source m o = Source { pull :: m (Maybe (o, Source m o)) }
+
+type Transducer m i o = Source m i -> Source m o
 
 class Origin a where
   type (Context a) :: * -> *
@@ -22,3 +25,20 @@ consume source =
 drain :: Monad m => Source m o -> m ()
 drain source =
   pull source >>= maybe (return ()) (drain . snd)
+
+take :: Monad m => Int -> Transducer m [i] [i]
+take i source = Source $ pull source >>= \m-> case m of
+  Nothing -> return Nothing
+  Just xs -> undefined
+
+head :: Monad m => Transducer m [i] i
+head source = pull source >>= \m-> case m of
+  --Just ([],   source') -> head source'
+  --Just (x:xs, source') -> Source $ return $ Just (x, pushBack xs source')
+  _                    -> exhausted
+
+pushBack         :: Monad m => o -> Source m o -> Source m o
+pushBack o source = Source $ return $ Just (o, source)
+
+exhausted        :: Monad m => Source m o
+exhausted         = Source $ return Nothing
