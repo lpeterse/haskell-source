7e7b8edd1d59eadeedcc7cd88a7faf977e1471dc
Lars Petersen <info@lars-petersen.net>
2016-04-01 22:55:27 +0200

    Added repeat and replicate.

--- src/Data/Source.hs
+++ src/Data/Source.hs
@@ -4,6 +4,8 @@ module Data.Source where
 import Control.Monad
 import System.IO
 
+import Prelude hiding ( repeat, replicate )
+
 -- |
 --
 -- * A source that once returned Nothing must always return Nothing in the future
@@ -15,20 +17,25 @@ data Yield m a
    | Exhaustion
    | Termination
 
-drain    :: Monad m => Source m a -> m ()
-drain     = (=<<) f
-  where
-    f (Yield _ src) = drain src
-    f _             = return ()
+drain        :: Monad m => Source m a -> m ()
+drain         = let f (Yield _ src) = drain src
+                    f _             = return ()
+                in  (=<<) f
 
-peek     :: Monad m => Source m a -> m (Maybe a, Source m a)
-peek      = fmap f
-  where
-    f (Yield a src') = (Just a, return $ Yield a $ prepend a src')
-    f x              = (Nothing, return x)
+peek         :: Monad m => Source m a -> m (Maybe a, Source m a)
+peek          = let f (Yield a sa) = (Just a, return $ Yield a $ prepend a sa)
+                    f x            = (Nothing, return x)
+                in  fmap f
 
-prepend  :: Monad m => a -> Source m a -> Source m a
-prepend a = return . Yield a
+prepend      :: Monad m => a -> Source m a -> Source m a
+prepend     a = pure . Yield a
+
+repeat       :: Monad m => a -> Source m a
+repeat      a = pure $ Yield a $ repeat a
+
+replicate    :: (Monad m, Integral i) => Int -> a -> Source m a
+replicate 0 _ = pure Termination
+replicate i a = pure $ Yield a $ replicate (pred i) a
 
 instance Monad m => Functor (Yield m) where
   fmap f (Yield a src)       = Yield (f a) (fmap f <$> src)
