36fd39c7de0414a26f21795f77bfd4b268b5869e
Lars Petersen <info@lars-petersen.net>
2016-04-02 15:31:40 +0200

    Added test-wise bindings for bytestring.

--- src/Data/Source.hs
+++ src/Data/Source.hs
@@ -3,54 +3,68 @@ module Data.Source where
 import Control.Monad
 import Prelude hiding ( repeat, replicate )
 
-type Source     m a = m (Yield m a)
-type Transducer m a = Source m a -> Source m a
+type Source     m a   = m (Yield m a)
+type Transducer m a b = Source m a -> Source m b
 
 data Yield m a
-   = Yield a (Source m a)
+   = Chunk a (Source m a)
    | Exhaustion
    | Termination
 
 drain        :: Monad m => Source m a -> m ()
-drain         = let f (Yield _ src) = drain src
-                    f _             = return ()
+drain         = let f (Chunk _ src) = drain src
+                    f _             = pure ()
                 in  (=<<) f
 
 peek         :: Monad m => Source m a -> Source m a
-peek          = let f (Yield a sa) = Yield a $ prepend a sa
+peek          = let f (Chunk a sa) = Chunk a $ prepend a sa
                     f ya           = ya
                 in  fmap f
 
 prepend      :: Monad m => a -> Source m a -> Source m a
-prepend     a = pure . Yield a
+prepend     a = pure . Chunk a
 
 repeat       :: Monad m => a -> Source m a
-repeat      a = pure $ Yield a $ repeat a
+repeat      a = pure $ Chunk a $ repeat a
 
 replicate    :: (Monad m, Integral i) => i -> a -> Source m a
 replicate 0 _ = pure Termination
-replicate i a = pure $ Yield a $ replicate (pred i) a
+replicate i a = pure $ Chunk a $ replicate (pred i) a
 
 instance Monad m => Functor (Yield m) where
-  fmap f (Yield a src)       = Yield (f a) (fmap f <$> src)
+  fmap f (Chunk a src)       = Chunk (f a) (fmap f <$> src)
   fmap _ Exhaustion          = Exhaustion
   fmap _ Termination         = Termination
 
 instance Monad m => Applicative (Yield m) where
-  pure                      a = Yield a (pure Termination)
-  Yield f sf  <*>  Yield a sa = Yield (f a) $ sf >>= \g-> liftM (g <*>) sa
+  pure                      a = Chunk a (pure Termination)
+  Chunk f sf  <*>  Chunk a sa = Chunk (f a) $ sf >>= \g-> liftM (g <*>) sa
   Exhaustion  <*>           _ = Exhaustion
   _           <*>  Exhaustion = Exhaustion
   Termination <*>           _ = Termination
   _           <*> Termination = Termination
 
 instance Monad m => Monoid (Yield m a) where
-  Yield a sa  `mappend`    yb = Yield a (f sa)
+  Chunk a sa  `mappend`    yb = Chunk a (f sa)
     where
       f sc = sc >>= \yc-> case yc of
-        Yield d sd  -> pure $ Yield d (f sd)
+        Chunk d sd  -> pure $ Chunk d (f sd)
         Exhaustion  -> pure Exhaustion
         Termination -> pure yb
   Exhaustion  `mappend`     _ = Exhaustion
   Termination `mappend`    yb = yb
   mempty                      = Termination
+
+mapChunk :: Functor m => (a -> Source m a -> Yield m b) -> Transducer m a b
+mapChunk f = fmap g
+  where
+    g (Chunk a sa) = f a sa
+    g Exhaustion   = Exhaustion
+    g Termination  = Termination
+
+whenChunk :: Monad m => (a -> Source m a -> Source m b) -> Transducer m a b
+whenChunk f = (=<<) g
+  where
+    g (Chunk a sa) = f a sa
+    g Exhaustion   = pure Exhaustion
+    g Termination  = pure Termination
---
+++ src/Data/Source/ByteString.hs
@@ -0,0 +1,28 @@
+module Data.Source.ByteString where
+
+import qualified Data.ByteString as BS
+import Data.Monoid
+import Data.Source
+import Data.Word
+import Prelude hiding ( head, null, take )
+
+head  :: Monad m => Transducer m BS.ByteString (Word8, Source m BS.ByteString)
+head   = mapChunk f
+  where
+    f a sa | BS.null a = Exhaustion
+           | otherwise = Chunk (BS.head a, prepend (BS.tail a) sa)
+                               (head $ prepend (BS.tail a) sa)
+
+take  :: (Monad m, Integral i) => i -> Transducer m BS.ByteString (BS.ByteString, Source m BS.ByteString)
+take i = whenChunk f
+  where
+    f a sa | j < i     = mapChunk g $ take (i - j) sa
+           | j == i    = pure $ Chunk (a, sa)
+                                      (take i sa)
+           | otherwise = pure $ Chunk (ta, prepend da sa)
+                                      (take i $ pure $ Chunk da $ prepend da sa)
+      where
+        j           = fromIntegral $ BS.length a
+        ta          = BS.take (fromIntegral i) a
+        da          = BS.drop (fromIntegral i) a
+        g (b, sb) _ = Chunk (a <> b, sb) (take i sb)
